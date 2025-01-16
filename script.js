17e9d7d567eced10bfda9da6a068c000a4145aa0
PLteddy <plt.sebdaoui@gmail.com>
2025-01-16 21:40:08 +0100

    7th commit
    Date: 1/01/2025
    Heure: 21h39
    Finition faites
    Le mail marche pas jsp pk

--- script.js
+++ script.js
@@ -61,3 +61,111 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
 });
+
+var cursor = {
+    delay: 8,
+    _x: 0,
+    _y: 0,
+    endX: (window.innerWidth / 2),
+    endY: (window.innerHeight / 2),
+    cursorVisible: true,
+    cursorEnlarged: false,
+    $dot: document.querySelector('.cursor-dot'),
+    $outline: document.querySelector('.cursor-dot-outline'),
+
+    init: function () {
+        // Set up element sizes
+        this.dotSize = this.$dot.offsetWidth;
+        this.outlineSize = this.$outline.offsetWidth;
+
+        // Show the cursor immediately
+        this.$dot.style.opacity = 1;
+        this.$outline.style.opacity = 1;
+
+        this.setupEventListeners();
+        this.animateDotOutline();
+    },
+
+    setupEventListeners: function () {
+        var self = this;
+
+        // Anchor hovering
+        document.querySelectorAll('a').forEach(function (el) {
+            el.addEventListener('mouseover', function () {
+                self.cursorEnlarged = true;
+                self.toggleCursorSize();
+            });
+            el.addEventListener('mouseout', function () {
+                self.cursorEnlarged = false;
+                self.toggleCursorSize();
+            });
+        });
+
+        // Click events
+        document.addEventListener('mousedown', function () {
+            self.cursorEnlarged = true;
+            self.toggleCursorSize();
+        });
+        document.addEventListener('mouseup', function () {
+            self.cursorEnlarged = false;
+            self.toggleCursorSize();
+        });
+
+        // Mouse movement
+        document.addEventListener('mousemove', function (e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+
+            self.endX = e.pageX;
+            self.endY = e.pageY;
+
+            self.$dot.style.top = self.endY + 'px';
+            self.$dot.style.left = self.endX + 'px';
+        });
+
+        // Hide/show cursor
+        document.addEventListener('mouseenter', function () {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+        });
+
+        document.addEventListener('mouseleave', function () {
+            self.cursorVisible = false;
+            self.toggleCursorVisibility();
+        });
+    },
+
+    animateDotOutline: function () {
+        var self = this;
+
+        self._x += (self.endX - self._x) / self.delay;
+        self._y += (self.endY - self._y) / self.delay;
+
+        self.$outline.style.top = self._y + 'px';
+        self.$outline.style.left = self._x + 'px';
+
+        requestAnimationFrame(this.animateDotOutline.bind(self));
+    },
+
+    toggleCursorSize: function () {
+        if (this.cursorEnlarged) {
+            this.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
+            this.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+        } else {
+            this.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
+            this.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+        }
+    },
+
+    toggleCursorVisibility: function () {
+        if (this.cursorVisible) {
+            this.$dot.style.opacity = 1;
+            this.$outline.style.opacity = 1;
+        } else {
+            this.$dot.style.opacity = 0;
+            this.$outline.style.opacity = 0;
+        }
+    }
+};
+
+cursor.init();
